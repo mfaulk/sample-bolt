1da4dfedc5bc85f263db59f64186318e4ed41e42
Matt Faulkner <mnfaulk@gmail.com>
2015-03-23 15:14:09 -0700

    Assumes /services/kafka is a directory of keys

--- app/bolt.py
+++ app/bolt.py
@@ -12,10 +12,11 @@ in_streams = ['fortune-cookie', 'topic-b']
 # IP:PORT of a Kafka broker. The typical port is 9092.
 KAFKA_BROKER_IP_PORT = os.getenv('KAFKA_BROKER', '192.168.86.10:9092')
 print "KAFKA BROKER: " + KAFKA_BROKER_IP_PORT
+logger.warn("KAFKA BROKER: " + KAFKA_BROKER_IP_PORT)
+
 kafka = KafkaClient(KAFKA_BROKER_IP_PORT)
 producer = SimpleProducer(kafka)
-consumer = KafkaConsumer(*in_streams, group_id="my_group",
-                         metadata_broker_list=[KAFKA_BROKER_IP_PORT])
+consumer = KafkaConsumer('fortune-cookie', group_id="my_group", metadata_broker_list=[KAFKA_BROKER_IP_PORT])
 
 for message in consumer:
     # message is raw byte string -- decode if necessary!
